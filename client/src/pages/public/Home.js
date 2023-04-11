25783284d56140c0068886907c35533bcbd82cb6
hip06 <khongpikten21@gmail.com>
2023-04-11 20:41:25 +0700

    v27

--- client/src/pages/public/Home.js
+++ client/src/pages/public/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Sidebar, Banner, BestSeller } from '../../components'
+import { Sidebar, Banner, BestSeller, DealDaily } from '../../components'
 
 
 const Home = () => {
@@ -7,11 +7,11 @@ const Home = () => {
     return (
         <>
             <div className='w-main flex'>
-                <div className='flex flex-col gap-5 w-[20%] flex-auto'>
+                <div className='flex flex-col gap-5 w-[25%] flex-auto'>
                     <Sidebar />
-                    <span>Deal daily</span>
+                    <DealDaily />
                 </div>
-                <div className='flex flex-col gap-5 pl-5 w-[80%] flex-auto'>
+                <div className='flex flex-col gap-5 pl-5 w-[75%] flex-auto'>
                     <Banner />
                     <BestSeller />
                 </div>
