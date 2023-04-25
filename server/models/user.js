2a94c9ab0cd94a6350b498c0724030dd4e0ae554
hip06 <khongpikten21@gmail.com>
2023-04-25 23:06:30 +0700

    v33

--- server/models/user.js
+++ server/models/user.js
@@ -51,7 +51,10 @@ var userSchema = new mongoose.Schema({
     },
     passwordResetExpires: {
         type: String
-    }
+    },
+    registerToken: {
+        type: String
+    },
 }, {
     timestamps: true
 });
