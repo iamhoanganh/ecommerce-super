887e2786b971c17864e83a6f5998cd1fb973b3fb
hip06 <khongpikten21@gmail.com>
2023-06-07 20:35:40 +0700

    v46

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -30,21 +30,24 @@ const getProducts = asyncHandler(async (req, res) => {
     let queryString = JSON.stringify(queries)
     queryString = queryString.replace(/\b(gte|gt|lt|lte)\b/g, macthedEl => `$${macthedEl}`)
     const formatedQueries = JSON.parse(queryString)
-
+    let colorQueryObject = {}
     // Filtering
     if (queries?.title) formatedQueries.title = { $regex: queries.title, $options: 'i' }
     if (queries?.category) formatedQueries.category = { $regex: queries.category, $options: 'i' }
-    if (queries?.color) formatedQueries.color = { $regex: queries.color, $options: 'i' }
-    let queryCommand = Product.find(formatedQueries)
+    if (queries?.color) {
+        delete formatedQueries.color
+        const colorArr = queries.color?.split(',')
+        const colorQuery = colorArr.map(el => ({ color: { $regex: el, $options: 'i' } }))
+        colorQueryObject = { $or: colorQuery }
+    }
+    const q = { ...colorQueryObject, ...formatedQueries }
+    let queryCommand = Product.find(q)
 
     // Sorting
-
-    // acb,efg => [abc,efg] => abc efg
     if (req.query.sort) {
         const sortBy = req.query.sort.split(',').join(' ')
         queryCommand = queryCommand.sort(sortBy)
     }
-
 
     // Fields limiting
     if (req.query.fields) {
@@ -53,11 +56,6 @@ const getProducts = asyncHandler(async (req, res) => {
     }
 
     // Pagination
-    // limit: số object lấy về 1 gọi API
-    // skip: 2
-    // 1 2 3 ... 10
-    // +2 => 2
-    // +dsdsad => NaN
     const page = +req.query.page || 1
     const limit = +req.query.limit || process.env.LIMIT_PRODUCTS
     const skip = (page - 1) * limit
@@ -66,7 +64,7 @@ const getProducts = asyncHandler(async (req, res) => {
     // Số lượng sp thỏa mãn điều kiện !== số lượng sp trả về 1 lần gọi API
     queryCommand.exec(async (err, response) => {
         if (err) throw new Error(err.message)
-        const counts = await Product.find(formatedQueries).countDocuments()
+        const counts = await Product.find(q).countDocuments()
         return res.status(200).json({
             success: response ? true : false,
             counts,
