5bad2f08d4a1c200004f4c5428eba1fca227e7c0
hip06 <khongpikten21@gmail.com>
2023-10-25 17:57:59 +0700

    v86

--- client/src/components/sidebar/MemberSidebar.js
+++ client/src/components/sidebar/MemberSidebar.js
@@ -5,6 +5,7 @@ import { NavLink, Link } from 'react-router-dom'
 import clsx from 'clsx'
 import { AiOutlineCaretDown, AiOutlineCaretRight } from 'react-icons/ai'
 import { useSelector } from 'react-redux'
+import { TbArrowForwardUp } from 'react-icons/tb'
 
 const activedStyle = 'px-4 py-2 flex items-center gap-2  bg-blue-500 text-gray-100'
 const notActivedStyle = 'px-4 py-2 flex items-center gap-2  hover:bg-blue-100'
@@ -56,7 +57,7 @@ const MemberSidebar = () => {
                         </div>}
                     </Fragment>
                 ))}
-                <NavLink to={'/'} className={clsx(notActivedStyle)}>Go home</NavLink>
+                <NavLink to={'/'} className={clsx(notActivedStyle)}><TbArrowForwardUp size={18} />Go home</NavLink>
             </div>
         </div>
     )
--- client/src/pages/member/Personal.js
+++ client/src/pages/member/Personal.js
@@ -20,6 +20,7 @@ const Personal = () => {
             mobile: current?.mobile,
             email: current?.email,
             avatar: current?.avatar,
+            address: current?.address,
         })
     }, [current])
     // const handleFile = async () => {
@@ -30,7 +31,6 @@ const Personal = () => {
     // useEffect(() => {
     //     if (watch('avatar') && isDirty) handleFile()
     // }, [watch('avatar')])
-    console.log(watch('avatar'))
     const handleUpdateInfor = async (data) => {
         const formData = new FormData()
         if (data.avatar.length > 0) formData.append('avatar', data.avatar[0])
@@ -89,6 +89,15 @@ const Personal = () => {
                             message: 'Phone invalid.'
                         }
 
+                    }}
+                />
+                <InputForm
+                    label='Address'
+                    register={register}
+                    errors={errors}
+                    id='address'
+                    validate={{
+                        required: 'Need fill this field',
                     }}
                 />
                 <div className='flex items-center gap-2'>
