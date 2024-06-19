59383d9c64f322dc6accc3853f8ff3dd7f9e1e42
Hoàng Anh <46701135+iamhoanganh@users.noreply.github.com>
2024-06-19 08:05:15 +0000

    [upd] theming & dark mode toggle for app

--- client-next/src/app/(auth)/login/page.tsx
+++ client-next/src/app/(auth)/login/page.tsx
@@ -1,5 +1,23 @@
+'use client'
+import { ModeToggle } from "@/components/mode-toggle";
+import { useEffect, useState } from "react";
+
 export default function LoginPage() {
+    const [email, setEmail] = useState("hoanganh@gmail.com")
+    useEffect(() => {
+        const idTimeOut = setTimeout(() => {
+            setEmail("quoac quoac")
+        }, 5000)
+        return () => {
+            clearTimeout(idTimeOut)
+        }
+    }, [])
+    
     return (
-        <div>Login Page</div>
+        <>
+            <div>Login Page</div>
+            <span>{email}</span>
+            <ModeToggle />
+        </>
     );
 }
