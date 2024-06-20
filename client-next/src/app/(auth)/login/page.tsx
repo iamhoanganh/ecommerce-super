a7a06e898d7d5cd30544d5493a7eeec1f483624d
Hoàng Anh <46701135+iamhoanganh@users.noreply.github.com>
2024-06-20 11:08:10 +0000

    [upd] add toast and handle submit login

--- client-next/src/app/(auth)/login/page.tsx
+++ client-next/src/app/(auth)/login/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import LoginForm from "@/components/login-form";
+import LoginForm from './login-form';
 export default function LoginPage() {
 
     return (
         <>
             <h2 className="text-center text-xl font-semibold">Đăng Nhập</h2>
             <div className="flex mx-auto justify-center">
-                <LoginForm/>
+                <LoginForm />
             </div>
         </>
     );
